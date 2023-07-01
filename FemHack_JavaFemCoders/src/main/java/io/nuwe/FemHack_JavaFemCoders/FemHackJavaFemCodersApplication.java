b65c4846a0ba2dfec055eb71960e33f380412b73
montseliz <montse.liz92@gmail.com>
2023-07-01 10:05:07 +0200

    structure project

---
+++ FemHack_JavaFemCoders/src/main/java/io/nuwe/FemHack_JavaFemCoders/FemHackJavaFemCodersApplication.java
@@ -0,0 +1,13 @@
+package io.nuwe.FemHack_JavaFemCoders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FemHackJavaFemCodersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FemHackJavaFemCodersApplication.class, args);
+	}
+
+}
---
+++ FemHack_JavaFemCoders/src/test/java/io/nuwe/FemHack_JavaFemCoders/FemHackJavaFemCodersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nuwe.FemHack_JavaFemCoders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FemHackJavaFemCodersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
