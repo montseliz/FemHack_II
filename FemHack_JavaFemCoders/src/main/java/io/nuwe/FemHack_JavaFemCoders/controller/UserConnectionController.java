50feeb7c058958edc10c3d2d32f09c9568325a64
montseliz <montse.liz92@gmail.com>
2023-07-01 23:37:16 +0200

    imports unused

--- FemHack_JavaFemCoders/src/main/java/io/nuwe/FemHack_JavaFemCoders/controller/UserConnectionController.java
+++ FemHack_JavaFemCoders/src/main/java/io/nuwe/FemHack_JavaFemCoders/controller/UserConnectionController.java
@@ -1,6 +1,5 @@
 package io.nuwe.FemHack_JavaFemCoders.controller;
 
-import io.nuwe.FemHack_JavaFemCoders.model.dto.UserConnectionDTO;
 import io.nuwe.FemHack_JavaFemCoders.model.exceptions.ConnectionsNotFoundException;
 import io.nuwe.FemHack_JavaFemCoders.model.service.UserConnectionService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,9 +12,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
 
 @RestController
 @RequiredArgsConstructor
