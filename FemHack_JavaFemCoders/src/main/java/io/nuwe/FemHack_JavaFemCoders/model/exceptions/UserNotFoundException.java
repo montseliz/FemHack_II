bbf5f5091fcbd8dc1e7e6ab60dd3278160971c57
montseliz <montse.liz92@gmail.com>
2023-07-02 14:05:42 +0200

    little correction in exceptions

--- FemHack_JavaFemCoders/src/main/java/io/nuwe/FemHack_JavaFemCoders/model/exceptions/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package io.nuwe.FemHack_JavaFemCoders.model.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
